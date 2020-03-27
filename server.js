8711a389af91e9bd75bfc21bfa914933249ce149
Teresa Liu <kuas033@gmail.com>
2020-03-26 20:59:16 -0700

    fix index.js bug

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const mongoose = require(`mongoose`);
 const PORT = process.env.PORT || 3000;
 
 const db = require(`./models`);
-const apiRoutes = require(`./routes/apiRoutes`);
-const htmlRoutes = require(`./routes/htmlRoutes`);
+const path = require(`path`);
 
 const app = express();
 
@@ -18,7 +17,7 @@ app.use(express.json());
 app.use(express.static(`public`));
 
 mongoose.connect(
-  process.env.MONGODB_URI || `mongodb://localhost/custommethoddb`,
+  process.env.MONGODB_URI || `mongodb://localhost/workoutdb`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -27,10 +26,56 @@ mongoose.connect(
   }
 );
 
-apiRoutes(app);
-htmlRoutes(app);
-
-
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/exercise.html'));
+});
+
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/stats.html'));
+});
+
+
+app.get('/api/workouts', (req, res) => {
+  db.Workout.find({})
+      .then(dbWorkout => {
+          res.json(dbWorkout);
+      })
+      .catch(err => {
+          res.status(400).json(err);
+      });
+});
+app.get(`/api/workouts/range`, (req, res) => {
+  db.Workout.find({})
+      .then(dbWorkout => {
+          res.json(dbWorkout);
+      })
+      .catch(err => {
+          res.json(err);
+      });
+});
+app.post(`/api/workouts`, (req, res) => {
+  db.Workout.create(req.body)
+      .then(dbWorkout => {
+          res.json(dbWorkout);
+      })
+      .catch(err => {
+          res.json(err);
+      });
+});
+app.put('/api/workouts/:id', (req, res) => {
+  db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { exercises: req.body } }
+  )
+      .then(results => res.json(results))
+      .catch(err => res.json(err))
 });
